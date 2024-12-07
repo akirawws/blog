7a046504d7e438653b613b98f271eec48355986f
akirawws <darkkyn813@gmail.com>
2024-12-07 23:38:24 +0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -29,3 +29,24 @@ window.addEventListener('load', function() {
     document.body.style.overflow = 'auto'; 
   }, 2000); 
 });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const snowContainer = document.getElementById('snowflakes');
+    const snowflakeCount = 50; 
+
+    for (let i = 0; i < snowflakeCount; i++) {
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake';
+      snowflake.innerHTML = '❄'; 
+      snowflake.style.left = Math.random() * 200 + 'vw';
+      snowflake.style.fontSize = Math.random() * 1.5 + 0.5 + 'em';
+      snowflake.style.animationDuration = Math.random() * 5 + 5 + 's';
+      snowflake.style.animationDelay = Math.random() * 5 + 's';
+      snowflake.style.setProperty('--horizontal-shift', Math.random() * 2 - 1); 
+
+      snowContainer.appendChild(snowflake);
+    }
+  });
+
